70bfccaa2d6c92f74ebe6fb85bc711a6e9ed5349
Alec Chapman <abchapman93@gmail.com>
2020-06-16 12:36:12 -0600

    add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_namespace_packages
+
+# read the contents of the README file
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="medspacy",
+    version="0.0.1",
+    description="Library for clinical NLP with spaCy.",
+    author="medSpaCy",
+    author_email="medspacy.dev@gmail.com",
+    packages=["medspacy"],
+    install_requires=["spacy>=2.2.2"],
+    long_description=long_description,
+    long_description_content_type="text/markdown"
+)

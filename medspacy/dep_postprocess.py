260e250057af74736663191a8fb2b805aa745166
Alec Chapman <abchapman93@gmail.com>
2020-11-06 18:04:19 -0700

    update notebooks and documentation for new packages

--- medspacy/dep_postprocess.py
+++
@@ -1,6 +0,0 @@
-from nlp_postprocessor import (
-    Postprocessor,
-    PostprocessingRule,
-    PostprocessingPattern,
-)
-from nlp_postprocessor import postprocessing_functions
--- medspacy/dep_preprocess.py
+++
@@ -1,1 +0,0 @@
-from nlp_preprocessor import Preprocessor, PreprocessingRule

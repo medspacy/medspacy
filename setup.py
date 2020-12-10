206ee6fd2fc4596b0b9164eaa6e8be87e4e9f390
burgersmoke <burgersmoke@gmail.com>
2020-12-09 17:40:26 -0700

    Trying different syntax to force QuickUMLS to install via GitHub.

--- setup.py
+++ setup.py
@@ -9,15 +9,16 @@ this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
-platform_dependency_links = []
+dependency_links = []
 if platform.startswith('win'):
     print('Not installing QuickUMLS for Windows since it currently requires conda (as opposed to just pip)')
 else:
     # Using a trick from StackOverflow to set an impossibly high version number
     # to force getting latest from GitHub as opposed to PyPi
     # since QuickUMLS has not made a release with some recent MedSpacy contributions...
-    platform_dependency_links.append('https://github.com/Georgetown-IR-Lab/QuickUMLS/tarball/master#egg=999.0.0')
-
+    quickumls_github_url = 'git+https://github.com/Georgetown-IR-Lab/QuickUMLS/tarball/master#egg=999.0.0'
+    dependency_links.append(quickumls_github_url)
+    print('Attempting to install quickumls from GitHub at : {}'.format(quickumls_github_url))
 
 # function to recursively get files for resourcee
 def package_files(directory):
@@ -55,7 +56,7 @@ setup(
         "PyRuSH>=1.0.3.5",
         "jsonschema"
     ],
-    dependency_links = platform_dependency_links,
+    dependency_links = dependency_links,
     long_description=long_description,
     long_description_content_type="text/markdown",
     package_data={"medspacy": resource_files},

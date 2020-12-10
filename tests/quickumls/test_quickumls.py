db81393330a57bb9a9fa272821f20f9535713b45
burgersmoke <burgersmoke@gmail.com>
2020-12-09 17:04:54 -0700

    Adding initial pieces to enable QuickUMLS (for Linux and MacOS at least) and set up initial testing.  Testing on Windows will currently pass if the quickumls has not been installed.  Windows installation of QuickUMLS will involve manual installation steps with conda.

---
+++ tests/quickumls/test_quickumls.py
@@ -0,0 +1,70 @@
+import spacy
+import warnings
+from sys import platform
+
+import medspacy
+
+class TestQuickUMLS:
+
+    @staticmethod
+    def can_test_quickumls():
+        if platform.startswith("win"):
+            import imp
+            try:
+                imp.find_module('quickumls')
+                found = True
+            except ImportError:
+                found = False
+
+            if not found:
+                # we're done here for now...
+                return False
+
+        return True
+
+    def test_initialize_pipeline(self):
+        """
+        Test that a pipeline with a QuickUMLS component can be loaded in medpacy
+        NOTE: Currently this is only available by default in Linux and MacOS
+            Windows requires additional steps, but this will test capability on Windows
+            if these manual steps are followed
+        """
+
+        # let's make sure that this pipe has been initialized
+        # At least for MacOS and Linux which are currently supported...
+        if not TestQuickUMLS.can_test_quickumls():
+            return
+
+        # allow default QuickUMLS (very small sample data) to be loaded
+        nlp = medspacy.load(enable=["quickumls"])
+        assert nlp
+
+        quickumls = nlp.get_pipe("QuickUMLS matcher")
+        assert quickumls
+        # this is a member of the QuickUMLS algorithm inside the component
+        assert quickumls.quickumls
+        # Check that the simstring database exists
+        assert quickumls.quickumls.ss_db
+
+    def test_quickumls_extractions(self):
+        """
+        Test that extractions can be performed using the very small (<100 concept) UMLS sample resources
+        """
+
+        # let's make sure that this pipe has been initialized
+        # At least for MacOS and Linux which are currently supported...
+        if not TestQuickUMLS.can_test_quickumls():
+            return
+
+        # allow default QuickUMLS (very small sample data) to be loaded
+        nlp = medspacy.load(enable=["quickumls"])
+        quickumls = nlp.get_pipe("QuickUMLS matcher")
+
+        # TODO -- Consider moving this and other extraction tests to separate tests from loading
+        doc = nlp('Decreased dipalmitoyllecithin content found in lung specimens')
+
+        assert len(doc.ents) == 1
+
+        entity_spans = [ent.text for ent in doc.ents]
+
+        assert 'dipalmitoyllecithin' in entity_spans

f277497c8e9651808c7b3250e17aa4d706f7b716
MichalMalyska <malyskamichal@gmail.com>
2021-06-04 18:29:49 -0400

    add pysbd and update setup

--- medspacy/sentence_splitting.py
+++ medspacy/sentence_splitting.py
@@ -1,4 +1,5 @@
 from PyRuSH import PyRuSHSentencizer
+import pysbd
 from spacy.language import Language
 
 from os import path
@@ -12,3 +13,22 @@ def create_pyrush(nlp, name, pyrush_path=None):
         )
 
     return PyRuSHSentencizer(pyrush_path)
+
+@Language.factory("medspacy_pysbd")
+class PySBDSenteceSplitter:
+    def __init__(self, name, nlp, clean=False):
+        self.name = name
+        self.nlp = nlp
+        self.seg = pysbd.Segmenter(language="en", clean=clean,
+                                   char_span=True)
+
+    def __call__(self, doc):
+        """
+        Spacy component based on: https://github.com/nipunsadvilkar/pySBD improved to work with spacy 3.0
+        """
+        sents_char_spans = self.seg.segment(doc.text_with_ws)
+        start_token_ids = [sent.start for sent in sents_char_spans]
+        for token in doc:
+            token.is_sent_start = (True if token.idx
+                                   in start_token_ids else False)
+        return doc

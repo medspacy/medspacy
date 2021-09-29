6fdc3578123a7e83fd88ec4075a81edc4d84afcc
Kelly Peterson <burgersmoke@gmail.com>
2021-09-29 16:48:25 -0600

    Setting up a new test to execute (most) notebooks to ensure that they are all working as expected.  A few have been skipped since they had large resources to install, or manual steps to get them to run.  Some are currently failing so the next commit might fix some of these notebooks.  This addresses Issue #92.

---
+++ tests/test_notebooks.py
@@ -0,0 +1,48 @@
+import pytest
+
+import os
+import subprocess
+import tempfile
+from sys import platform
+
+class TestNotebooks:
+    def test_execute_example_notebooks(self):
+        successful_executions = 0
+
+        for root, dirs, files in os.walk(os.path.join(os.getcwd(), "notebooks")):
+            for file in files:
+                if file.endswith('.ipynb'):
+
+                    # Skip this one since it has some large dependencies and downloads
+                    if 'using-pretrained-models' in file.lower():
+                        continue
+
+                    # Skip this one since it has some large dependencies and manual downloads
+                    if '12-io' in file.lower():
+                        continue
+
+                    # Skip this on Windows since there are other pieces which must be installed
+                    # manually on this platform for now
+                    if platform.startswith("win") and 'quickumls' in file.lower():
+                        continue
+
+                    notebook_full_path = os.path.join(root, file)
+
+                    # do not run on checkpoints of notebooks!
+                    if '.ipynb_checkpoints' in notebook_full_path.lower():
+                        continue
+
+                    # Following a pattern from here:
+                    # https://github.com/ghego/travis_anaconda_jupyter/blob/master/test_nb.py
+                    with tempfile.NamedTemporaryFile(suffix=".ipynb", delete=False) as fout:
+                        args = ["jupyter", "nbconvert", "--to", "notebook", "--execute",
+                                "--ExecutePreprocessor.timeout=1000",
+                                # Need to specify a kernel so we don't use the default
+                                "--ExecutePreprocessor.kernel_name=python3",
+                                "--output", fout.name, notebook_full_path]
+                        subprocess.check_call(args)
+                        successful_executions += 1
+
+        # Make sure we executed something
+        # (ensure that the logic above is still actually running notebooks)
+        assert successful_executions > 0

85138e964a2518753caa375ebf584f65caa2c59f
Mengke Hu <mengke.hu@gmail.com>
2022-09-14 16:46:50 -0600

    change medspacy/sentence_splitting.py to match updated PyRuSH; update test_medspacy.py and test_extensions.py;add notebook test/test_medspayc.ipynb;test_notebooks.py needs to be fixed.

--- medspacy/sentence_splitting.py
+++ medspacy/sentence_splitting.py
@@ -6,11 +6,11 @@ from os import path
 from pathlib import Path
 
 
-@Language.factory("medspacy_pyrush")
-def create_pyrush(nlp, name, pyrush_path=None):
-    if pyrush_path is None:
-        pyrush_path = path.join(Path(__file__).resolve().parents[1], "resources", "rush_rules.tsv")
-    return PyRuSHSentencizer(pyrush_path)
+#@Language.factory("medspacy_pyrush")
+#def create_pyrush(nlp, name, pyrush_path=None):
+#    if pyrush_path is None:
+#        pyrush_path = path.join(Path(__file__).resolve().parents[1], "resources", "rush_rules.tsv")
+#    return PyRuSHSentencizer(pyrush_path)
 
 
 @Language.factory("medspacy_pysbd")

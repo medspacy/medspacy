fcdcf17c2a039a2ebaf476db970f8ce905dbf46a
Mengke Hu <mengke.hu@gmail.com>
2022-09-16 14:51:58 -0600

    add notebook Untitled.ipynb;fix the dir path in test_note_books.py; find the problem notebook:medspacy/notebooks/02-Tokenizing-and-Sentence-Splitting.ipynb

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -8,7 +8,7 @@ from sys import platform
 class TestNotebooks:
     def test_execute_example_notebooks(self):
         successful_executions = 0
-
+        os.chdir("../") #move to medspacy folder
         for root, dirs, files in os.walk(os.path.join(os.getcwd(), "notebooks")):
             for file in files:
                 if file.endswith('.ipynb'):

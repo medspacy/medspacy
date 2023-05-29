c00833c9ce9d7a19742f2a0cce6a39a992757db8
Jianlin Shi <jianlins@users.noreply.github.com>
2023-05-28 20:07:55 -0600

    test version

--- medspacy/__init__.py
+++ medspacy/__init__.py
@@ -1,6 +1,6 @@
-# from . import components
-# from ._extensions import set_extensions, get_extensions, get_doc_extensions, get_span_extensions, get_token_extensions
+from . import components
+from ._extensions import set_extensions, get_extensions, get_doc_extensions, get_span_extensions, get_token_extensions
 from ._version import __version__
-# from .util import load
+from .util import load
 
-# set_extensions()
+set_extensions()
--- medspacy/_version.py
+++ medspacy/_version.py
@@ -1,1 +1,3 @@
-__version__ = "1.1.2"
+import os
+# use this to satisfy toml version configure without import dependencies
+__version__ = open(os.path.join(os.path.dirname(__file__), 'VERSION')).read()

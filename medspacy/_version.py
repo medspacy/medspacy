df544c34ffff3f212fe414eb8fd2a8569262bd95
Jianlin Shi <jianlins@users.noreply.github.com>
2023-05-28 20:25:18 -0600

    test version

--- medspacy/_version.py
+++ medspacy/_version.py
@@ -1,3 +1,3 @@
 import os
-# use this to satisfy toml version configure without import dependencies
+# use this to satisfy toml dynamic version configure without import othere dependencies
 __version__ = open(os.path.join(os.path.dirname(__file__), 'VERSION')).read()

c1b1c34a5e8266c5a8563fea4175c2dca7fa4d15
vhabhsgieraa <aaron.giera@va.gov>
2024-02-16 17:14:27 -0500

    feat: Add section header senting as opt in option

--- medspacy/section_detection/sectionizer.py
+++ medspacy/section_detection/sectionizer.py
@@ -66,6 +66,7 @@ class Sectionizer:
         span_attrs: Union[
             Literal["default"], Dict[str, Dict[str, Any]], None
         ] = "default",
+        apply_sentence_boundary: bool = False,
     ):
         """
         Create a new Sectionizer component.
@@ -101,6 +102,8 @@ class Sectionizer:
                 "group" will modify attributes of spans in the span group specified by `span_group_name`.
             span_group_name: The name of the span group used when `input_span_type` is "group". Default is
                 "medspacy_spans".
+            apply_sentence_boundary: Optionally end sentence before and after section header boundary. This ensures
+                the section header is considered its own sentence.
         """
         self.nlp = nlp
         self.name = name
@@ -113,6 +116,7 @@ class Sectionizer:
         self._parent_required = {}
         self._input_span_type = input_span_type
         self._span_group_name = span_group_name
+        self._apply_sentence_boundary = apply_sentence_boundary
 
         self.__matcher = MedspacyMatcher(
             nlp, name=name, phrase_matcher_attr=phrase_matcher_attr
@@ -420,10 +424,13 @@ class Sectionizer:
                 # parent sections were never set, so parent_idx does not exist
                 (match_id, start, end) = match
 
-            # Section headers should be considered the start of a sentence
-            doc[start].sent_start = True
-            # Text following the header should also be considered a new sentence
-            doc[end+1].sent_start = True
+            # Make section header its own sentence
+            if self._apply_sentence_boundary:
+                # Section headers should be considered the start of a sentence
+                doc[start].sent_start = True
+                # Text following the header should also be considered a new sentence
+                if end+1 < len(doc):
+                    doc[end+1].sent_start = True
 
             rule = self.__matcher.rule_map[self.nlp.vocab.strings[match_id]]
             category = rule.category

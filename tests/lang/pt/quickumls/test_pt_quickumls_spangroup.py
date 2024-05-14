a0ff7f84f835afac5730840b205c8f08a3d72634
burgersmoke <burgersmoke@gmail.com>
2024-05-14 09:27:57 -0600

    Fixed portuguese test to pass after fixing an upstream issue where a language naming for portuguese (i.e., portoguese) did not match NLTK stopword naming and QuickUMLS would fail

---
+++ tests/lang/pt/quickumls/test_pt_quickumls_spangroup.py
@@ -0,0 +1,62 @@
+import os, sys
+# recent pytest failed because of project directory is not included in sys.path somehow, might due to other configuration issue. Add this for a temp solution
+sys.path.append(os.getcwd())
+import spacy
+import warnings
+from sys import platform
+import pytest
+from os import path
+from pathlib import Path
+
+import medspacy
+
+from quickumls import spacy_component
+
+from medspacy.util import get_quickumls_demo_dir
+
+MEDSPACY_DEFAULT_SPAN_GROUP_NAME = "medspacy_spans"
+
+LANGUAGE = 'pt'
+
+class TestQuickUMLSSpanGroupPT:
+    def test_span_groups_pt(self):
+        """
+        Test that span groups can bs used as a result type (as opposed to entities)
+        """
+
+        # allow default QuickUMLS (very small sample data) to be loaded
+
+        nlp = spacy.blank(LANGUAGE)
+
+        # Configure the QuickUMLS component for these tests on SpanGroups
+        # Some allow overlaps, but this config will currently work for all of the tests below
+        nlp.add_pipe(
+            "medspacy_quickumls",
+            config={
+                "threshold": 0.7,
+                "result_type": "group",
+                # do not constrain to the best match for overlapping
+                "best_match": False,
+                "quickumls_fp": get_quickumls_demo_dir(LANGUAGE),
+            },
+        )
+
+        concept_term = "Dipalmitoil-Lecitina"
+
+        text = "{} é um conceito".format(concept_term)
+
+        doc = nlp(text)
+
+        print('tokens:')
+        for token in doc:
+            print(token.text)
+
+        assert len(doc.ents) == 0
+
+        assert len(doc.spans[MEDSPACY_DEFAULT_SPAN_GROUP_NAME]) == 1
+
+        span = doc.spans[MEDSPACY_DEFAULT_SPAN_GROUP_NAME][0]
+
+        assert len(span._.umls_matches) > 0
+
+
